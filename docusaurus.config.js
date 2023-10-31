d4a3f93c113c0712769abb7b30ba533db5e92291
v1xingyue <qixingyue@gmail.com>
2023-10-31 18:16:51 +0800

    github link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,7 +76,7 @@ const config = {
           },
           { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/CreatorsDAO/plasmo-co-learn",
             label: "GitHub",
             position: "right",
           },
