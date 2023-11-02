3841fdcf36d4ddbe60b62c6a6842ca70a1ef054f
v1xingyue <qixingyue@gmail.com>
2023-11-02 14:21:37 +0800

    简介 1/3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
+  organizationName: "CreatorsDAO", // Usually your GitHub org/user name.
   projectName: "plasmo-co-learn", // Usually your repo name.
 
   onBrokenLinks: "throw",
