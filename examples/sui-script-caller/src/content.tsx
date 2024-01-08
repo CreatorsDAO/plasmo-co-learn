c54821146cb36c8192324cfdf9e5381c372f96f4
v1xingyue <qixingyue@gmail.com>
2024-01-08 17:59:24 +0800

    add dappStyle

--- examples/sui-script-caller/src/content.tsx
+++ examples/sui-script-caller/src/content.tsx
@@ -10,8 +10,9 @@ import { getFullnodeUrl } from "@mysten/sui.js/client"
 import { TransactionBlock } from "@mysten/sui.js/dist/cjs/builder"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import styleText from "data-text:./style.css"
+import dappStyle from "data-text:@mysten/dapp-kit/dist/index.css"
 import type { PlasmoCSConfig } from "plasmo"
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 import type { Script } from "~hooks/script"
 import { ScriptDatabase } from "~hooks/useScript"
@@ -46,7 +47,8 @@ export const getRootContainer = () =>
         clearInterval(checkInterval)
         const rootContainer = document.createElement("div")
         const style = document.createElement("style")
-        style.textContent = styleText
+        style.textContent = styleText + "\n\n" + dappStyle
+
         rootContainerParent.appendChild(style)
         rootContainerParent.appendChild(rootContainer)
         resolve(rootContainer)
@@ -56,6 +58,7 @@ export const getRootContainer = () =>
   })
 
 const MyApp = () => {
+  const iframeRef = useRef<HTMLIFrameElement>(null)
   const [display, updateDisplay] = useState(false)
   const { mutate: signTransactionBlock } = useSignAndExecuteTransactionBlock()
   const account = useCurrentAccount()
@@ -67,27 +70,32 @@ const MyApp = () => {
     updateDisplay(!display)
   }
 
-  const executeScript = async () => {
-    const txb = new TransactionBlock()
+  const executeScript = async () => {}
 
-    eval(script)
-    return signTransactionBlock(
-      { transactionBlock: txb, chain: account.chains[0] },
-      {
-        onSuccess: (data) => {
-          alert(JSON.stringify(data))
-          setTransaction(data.digest)
-        },
-        onError: (error) => {
-          console.log(error)
-          alert(error)
-        }
+  useEffect(() => {
+    window.addEventListener("message", (event) => {
+      const { type, transactionBytes } = event.data
+      if (type === "scriptExecuteResult") {
+        const txb = TransactionBlock.from(transactionBytes)
+        signTransactionBlock(
+          { transactionBlock: txb, chain: account.chains[0] },
+          {
+            onSuccess: (data) => {
+              alert(JSON.stringify(data))
+              setTransaction(data.digest)
+            },
+            onError: (error) => {
+              console.log(error)
+              alert(error)
+            }
+          }
+        )
       }
-    )
-  }
+    })
+  })
 
   return display ? (
-    <div className="plasmo-card plasmo-w-full  plasmo-bg-base-100 plasmo-shadow-xl plasmo-p-3 plasmo-z-50 plasmo-flex">
+    <div className="plasmo-card plasmo-w-full  plasmo-bg-base-100 plasmo-shadow-xl plasmo-p-3 plasmo-flex">
       <QueryClientProvider client={queryClient}>
         <SuiClientProvider networks={networkConfig}>
           <WalletProvider autoConnect={true}>
@@ -118,12 +126,11 @@ const MyApp = () => {
                   )
                 })}
               </select>
-
               <textarea
                 placeholder="Enter your script here!"
                 value={script}
                 onChange={(e) => updateScript(e.target.value)}
-                className="plasmo-mt-4 plasmo-w-full plasmo-h-64 plasmo-p-2 plasmo-text-sm plasmo-text-gray-700 plasmo-border plasmo-border-gray-300 plasmo-rounded-md focus:plasmo-outline-none focus:plasmo-ring-1 focus:plasmo-ring-blue-500 focus:plasmo-border-transparent"
+                className="plasmo-mt-4 plasmo-w-full plasmo-h-96 plasmo-p-2 plasmo-text-sm plasmo-text-gray-700 plasmo-border plasmo-border-gray-300 plasmo-rounded-md focus:plasmo-outline-none focus:plasmo-ring-1 focus:plasmo-ring-blue-500 focus:plasmo-border-transparent"
               />
               <button
                 className="plasmo-mt-3 plasmo-btn plasmo-btn-info"
@@ -135,7 +142,6 @@ const MyApp = () => {
                 onClick={toggleDisplay}>
                 Close Me!
               </button>
-
               {transction ? (
                 <span className="plasmo-ml-3">{transction}</span>
               ) : null}
@@ -146,7 +152,7 @@ const MyApp = () => {
     </div>
   ) : (
     <button
-      className="plasmo-z-50 plasmo-flex plasmo-fixed plasmo-top-32 plasmo-right-8 plasmo-btn plasmo-btn-info"
+      className="plasmo-flex plasmo-fixed plasmo-top-32 plasmo-right-8 plasmo-btn plasmo-btn-info"
       onClick={toggleDisplay}>
       SUI Script
     </button>
@@ -157,7 +163,11 @@ const PlasmoOverlay = () => {
   const queryClient = new QueryClient()
 
   return (
-    <div className="plasmo-fixed plasmo-top-28 plasmo-right-8">
+    <div
+      style={{
+        zIndex: 5000
+      }}
+      className="plasmo-fixed plasmo-top-28 plasmo-right-8">
       <QueryClientProvider client={queryClient}>
         <SuiClientProvider networks={networkConfig} defaultNetwork="mainnet">
           <WalletProvider autoConnect={true}>
